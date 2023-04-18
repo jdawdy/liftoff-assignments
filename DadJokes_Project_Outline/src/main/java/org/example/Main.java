421c223df425e15e453d44b3d1abcef4ea4b2396
jdawdy <jacobldawdy@gmail.com>
2023-04-17 19:52:42 -0500

    Added Outline

---
+++ DadJokes_Project_Outline/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+//	This project is designed to offer new dad jokes to the user. They will be able to like, dislike, and bookmark jokes they like. The user will also be able to fabricate their own joke and submit it to the public for view.
+//	Users will enjoy the ability to constantly find new jokes that will entertain them or others around them.
+//
+//  Features-
+//  Authentication
+//  Like/Dislike system
+//  Create joke
+//  Delete Joke
+//
+//  Technologies-
+//  JavaScript
+//  React
+//  MySQL
+//
+//  I will have to learn react for this project and using data from a public API and save new content to a SQL Database.
